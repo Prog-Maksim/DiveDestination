54b08d01841a7af9f334e0936d3f88300c6e12c7
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-06-01 12:19:48 +0300

    добавление скрипта возвращения данных

---
+++ DiveDestination/Scripts/GetDataPerson.cs
@@ -0,0 +1,30 @@
+﻿namespace DiveDestination.Scripts;
+
+public class GetDataPerson
+{
+    public static List<string> getIdLoginPhoneNumber(List<PersonData> data, string login)
+    {
+        foreach (var personData in data)
+        {
+            if (personData.loginNumber == login)
+            {
+                return new List<string> {personData.Id, personData.password, personData.Name};
+            }
+        }
+
+        throw new InvalidDataException("Пользователь под данным логином не найден!");
+    }
+    
+    public static List<string> getIdLoginEmailAddress(List<PersonData> data, string login)
+    {
+        foreach (var personData in data)
+        {
+            if (personData.loginEmail == login)
+            {
+                return new List<string> {personData.Id, personData.password, personData.Name};
+            }
+        }
+
+        throw new InvalidDataException("Пользователь под данным логином не найден!");
+    }
+}
