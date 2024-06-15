5f53d21062db933df7e5a52ed1001295349a9b72
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-06-15 22:05:55 +0300

    добавление контекста БД

---
+++ DiveDestination/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server_Test.Models;
+
+namespace DiveDestination;
+
+public class ApplicationContext: DbContext
+{
+    public DbSet<Persons> Persons { get; set; } = null!;
+    public DbSet<PassData> PassData { get; set; } = null!;
+    public DbSet<UserLevel> UserLevel { get; set; } = null!;
+    public DbSet<Posts> Posts { get; set; } = null!;
+    
+    public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
+    {
+        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
