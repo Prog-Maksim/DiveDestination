36299047ea6703f57ec18a3048249109bedc6336
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-06-16 14:35:11 +0300

    добавление endpoint обновление токена

--- DiveDestination/Controllers/AuthorizationController.cs
+++ DiveDestination/Controllers/AuthorizationController.cs
@@ -72,4 +72,77 @@ public class AuthorizationController(ILogger<AuthorizationController> logger, Ap
 
         return Problem(problem.Detail, null, problem.Status, problem.Title);
     }
+    
+    [HttpPost("refresh-token")]
+    public IActionResult RefreshToken([FromForm] string access_token)
+    {
+        try
+        {
+            var principal = GetPrincipalFromExpiredToken(access_token);
+            if (principal == null)
+            {
+                return BadRequest("Invalid token");
+            }
+
+            var newJwtToken = GenerateNewToken(principal);
+            return Ok(new
+            {
+                message = "токен успешно обновлен",
+                access_token = newJwtToken
+            });
+        }
+        catch
+        {
+            return BadRequest("Invalid token");
+        }
+    }
+    
+    private string GenerateNewToken(ClaimsPrincipal principal)
+    {
+        var claims = principal.Claims.ToList();
+        var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(5)),
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256)
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
+    
+    private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidAudience = AuthOptions.AUDIENCE,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey()
+        };
+
+        try
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+
+            if (jwtSecurityToken == null ||
+                !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
+                    StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new SecurityTokenException("Invalid token");
+            }
+
+            return principal;
+        }
+        catch (ArgumentException)
+        {
+            throw new SecurityTokenException("Invalid token");
+        }
+    }
 }
